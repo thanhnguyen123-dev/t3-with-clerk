bd1e3664b34bfdeaf619d59d22743e8851829b3d
Thanh Nguyen <lethanh300504@gmail.com>
2025-04-16 18:37:09 +1000

    update

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { SignOutButton } from "@clerk/nextjs";
+const page = async () => {
+  const user = await currentUser();
+  if (!user) {
+    redirect("/");
+  }
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>{user.primaryEmailAddress?.emailAddress}</p>
+      <SignOutButton />
+    </div>
+  );
+};
+
+export default page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,29 @@
-import { api, HydrateClient } from "@/trpc/server";
+"use client";
+
+import { SignInButton, useUser } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
 
 
 export default function Home() {
+  const router = useRouter();
+  const { isSignedIn } = useUser();
+  const { user } = useUser();
+  console.log("user: ", user);
+
+  if (user) {
+    router.push("/dashboard");
+  }
+  if (!user) {
+    return (
+      <main className="flex flex-col gap-4 min-h-screen items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <h1>Landing page</h1>
+        <SignInButton 
+          mode="modal"
+        />
+      </main>
+    );
+  }
   
 
-  return (
-    <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        
-      </main>
-    </HydrateClient>
-  );
+
 }
