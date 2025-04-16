69dfb6c3f6bc2aa0515ca74650f4093ded99b52e
Thanh Nguyen <lethanh300504@gmail.com>
2025-04-17 00:22:58 +1000

    template v1

--- src/app/(authenticated)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import SidebarLayout from "@/layouts/sidebar-layout";
-
-export default function AuthenticatedLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <SidebarLayout>{children}</SidebarLayout>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { SignInButton } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs/server";
 
 export default async function Home() {
