a0ee651f5b6b68183c680c88d7b4d1002e1eaccb
Thanh Nguyen <lethanh300504@gmail.com>
2025-04-16 18:19:37 +1000

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { api, HydrateClient } from "@/trpc/server";
+
+
+export default function Home() {
+  
+
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        
+      </main>
+    </HydrateClient>
+  );
+}
